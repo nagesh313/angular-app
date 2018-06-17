1ee09d24007b3daa5acb389a8b348d1a12f6d37e
Nagesh Mishra <nagesh3.13@gmail.com>
2018-06-17 19:31:11 +0530

    Fixing spec files error during build

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,14 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeroSearchComponent } from './hero-search.component';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
   let fixture: ComponentFixture<HeroSearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroSearchComponent ]
+      declarations: [ HeroSearchComponent ],
+      imports: [RouterTestingModule, HttpClientTestingModule]
     })
     .compileComponents();
   }));
